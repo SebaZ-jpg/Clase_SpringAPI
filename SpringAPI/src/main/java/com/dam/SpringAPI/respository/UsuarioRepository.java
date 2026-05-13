513453bd2b5ab345044b09b3946059af6905cb87
SebaZ-jpg <sebastianalonsoaceroguiop@gmail.com>
2026-05-13 11:10:56 +0200

    primer commit

---
+++ SpringAPI/src/main/java/com/dam/SpringAPI/respository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.dam.SpringAPI.respository;
+
+import com.dam.SpringAPI.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+}
